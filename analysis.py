356f2f866158ee04d55c0876f890201546e08585
nasoma <pythonantole@gmail.com>
2019-11-07 20:43:49 +0300

    Initial commit

---
+++ analysis.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import streamlit as st
+
+
+@st.cache()
+def population_voter_analysis():
+    df = pd.read_csv('county_list.csv').set_index('COUNTY').drop(['code', 'MALE', 'FEMALE', 'INTERSEX'], axis=1)
+    percentage_of_voters = df['VOTERS'] / df["TOTAL"] * 100
+    df.insert(0, "% Voters", percentage_of_voters)
+
+    highest_voters_percentage = df.sort_values(by=['% Voters'], ascending=False).rename(
+        columns={'TOTAL': 'TOTAL POPULATION', 'VOTERS': 'REGISTERED VOTERS'}).head(10)
+
+    lowest_voters_percentage = df.sort_values(by=['% Voters'], ascending=True).rename(
+        columns={'TOTAL': 'TOTAL POPULATION', 'VOTERS': 'REGISTERED VOTERS'}).head(10)
+    return highest_voters_percentage, lowest_voters_percentage
+
+@st.cache()
+def gender_ratios():
+    df = pd.read_csv('county_list.csv')
+    male_to_female = df['MALE'] / df['FEMALE']
+    df.insert(1, "Male To Female Ratio", male_to_female)
+    m_f = df.sort_values(by=['Male To Female Ratio'], ascending=False).drop(['code', 'VOTERS', 'INTERSEX'
+                                                                             ], axis=1).set_index('COUNTY').head(10)
+    female_to_male = df['FEMALE'] / df['MALE']
+    df.insert(1, "Female To Male Ratio", female_to_male)
+    f_m = df.sort_values(by=['Female To Male Ratio'], ascending=False).drop(['code', 'VOTERS', 'INTERSEX',
+                                                                             'Male To Female Ratio'], axis=1).set_index(
+        'COUNTY').head(10)
+    top_population = df.sort_values(by=['TOTAL'], ascending=False).drop(['code', 'VOTERS',
+                                                                         'Female To Male Ratio',
+                                                                         'Male To Female Ratio'], axis=1).set_index(
+        'COUNTY').head(10)
+    least_population = df.sort_values(by=['TOTAL'], ascending=False).drop(['code', 'VOTERS',
+                                                                           'Female To Male Ratio',
+                                                                           'Male To Female Ratio'], axis=1).set_index(
+        'COUNTY').tail(10)
+
+    return m_f, f_m, top_population, least_population
---
+++ app.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import locale
+from analysis import *
+
+locale.setlocale(locale.LC_ALL, 'en_US')
+
+voter_percentages = population_voter_analysis()
+male_female_ratios = gender_ratios()
+
+st.sidebar.markdown('#### Kenya 2019 Population Census Summary')
+
+#df = st.cache(pd.read_csv)('county_list.csv')
+df = pd.read_csv('county_list.csv')
+
+counties = st.sidebar.selectbox('Select county', df['COUNTY'], index=29)
+registered_voters = st.sidebar.checkbox("Compare With Voter Registration Data, (IEBC 2017)")
+
+st.sidebar.markdown(' #### Parts Of Tens')
+parts_of_tens = st.sidebar.selectbox('', ('Select Option...', '10 Most Populous Counties', '10 Least Populous Counties',
+                                            'Highest Male To Female Ratio', 'Highest Female To Male Ratio',
+                                            'Highest % Of Registered Voters', 'Lowest % Of Registered Voters'))
+
+if '10 Most Populous Counties' in parts_of_tens:
+    st.subheader('10 Most Populous Counties')
+    st.table(male_female_ratios[2])
+elif '10 Least Populous Counties' in parts_of_tens:
+    st.subheader('10 Least Populous Counties')
+    st.table(male_female_ratios[3])
+elif 'Highest Male To Female Ratio' in parts_of_tens:
+    st.subheader('Highest Male To Female ratio')
+    st.table(male_female_ratios[0])
+elif 'Highest Female To Male Ratio' in parts_of_tens:
+    st.subheader('Highest Female To Male Ratio')
+    st.table(male_female_ratios[1])
+elif 'Highest % Of Registered Voters' in parts_of_tens:
+    st.subheader('Highest % Of Registered Voters')
+    st.table(voter_percentages[0])
+elif 'Lowest % Of Registered Voters' in parts_of_tens:
+    st.subheader('Lowest % Of Registered Voters')
+    st.table(voter_percentages[1])
+else:
+    county_data = df.loc[df.COUNTY == counties]
+    total = county_data['TOTAL'].values[0]
+    voters = county_data['VOTERS'].values[0]
+    male = county_data['MALE'].values[0]
+    female = county_data['FEMALE'].values[0]
+    intersex = county_data['INTERSEX'].values[0]
+
+    st.subheader(counties + " County, Total Population: " + locale.format_string("%d", total, grouping=True))
+
+    st.text("Male: " + locale.format_string("%d", male, grouping=True))
+    st.text("Female: " + locale.format_string("%d", female, grouping=True))
+    st.text("Intersex: " + locale.format_string("%d", intersex, grouping=True))
+
+    if registered_voters:
+        '''
+        ### Voter Registration
+        '''
+        st.text('Total Voters: ' + locale.format_string("%d", voters, grouping=True))
+        voters_percentage = (voters / total) * 100
+
+        st.text("% of voters: " + str(round(voters_percentage, 2)))
+        hover_text = "% of voters: " + str(round(voters_percentage, 2))
+        voters_vs_pop = ['Population', 'Voters']
+        colors = ['burlywood', 'chocolate']
+        fig = go.Figure(data=[
+        go.Bar(x=voters_vs_pop, y=[total, voters], hovertext=['', hover_text], marker_color=colors)])
+        fig.update_layout(title_text='Population Against No. Of Registered Voters')
+        st.plotly_chart(fig)
+    else:
+        labels = ['Male', 'Female']
+        values = [male, female]
+        colors = ['burlywood', 'chocolate']
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+        fig.update_traces(marker=dict(colors=colors, line=dict(color='#F5F5DC', width=1)))
+        st.plotly_chart(fig)
+
+st.sidebar.markdown(' #### About')
+st.sidebar.info("This app uses data that is publicly available. It is not affiliated to IEBC or KNBS.")
+st.sidebar.info("Email: tazamadata@gmail.com")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
